8bc5c8dece30498bc3207ef8f1cc95750f4a6fe8
OmarAMonem <omar198091@beng.bu.edu.eg>
2022-09-10 19:32:17 +0200

    init commit

--- Src/MCAL/Inc/Nvic_Types.h
+++ Src/MCAL/Inc/Nvic_Types.h
@@ -28,93 +28,92 @@
 /**********************************************************************************************************************
  *  GLOBAL DATA TYPES AND STRUCTURES
  *********************************************************************************************************************/
-typedef enum
-{
-    NVIC_GPIO_PORT_A,
-    NVIC_GPIO_PORT_B,
-    NVIC_GPIO_PORT_C,
-    NVIC_GPIO_PORT_D,
-    NVIC_GPIO_PORT_E,
-    NVIC_UART_0,
-    NVIC_UART_1,
-    NVIC_SSI_0,
-    NVIC_I2C_0,
-    NVIC_PWM_0_FAULT,
-    NVIC_PWM_0_GEN_0,
-    NVIC_PWM_0_GEN_1,
-    NVIC_PWM_0_GEN_2,
-    NVIC_QEI_0,
-    NVIC_ADC_0_SEQ_0,
-    NVIC_ADC_0_SEQ_1,
-    NVIC_ADC_0_SEQ_2,
-    NVIC_ADC_0_SEQ_3,
-    NVIC_TIMER_WATCHDOG_0_1,
-    NVIC_16_32_TIMER_0_A,
-    NVIC_16_32_TIMER_0_B,
-    NVIC_16_32_TIMER_1_A,
-    NVIC_16_32_TIMER_1_B,
-    NVIC_16_32_TIMER_2_A,
-    NVIC_16_32_TIMER_2_B,
-    NVIC_ANALOG_COMPARATOR_0,
-    NVIC_ANALOG_COMPARATOR_1,
-    NVIC_SYSTEM_CONTROL = 28,
-    NVIC_FLASH_EEPROM_MEMORY_CONTROL,
-    NVIC_GPIO_PORT_F,
-    NVIC_UART_2 = 33,
-    NVIC_SSI_1,
-    NVIC_16_32_TIMER_3_A,
-    NVIC_16_32_TIMER_3_B,
-    NVIC_I2C_1,
-    NVIC_QEI_1,
-    NVIC_CAN_0,
-    NVIC_CAN_1,
-    NVIC_HIBERNATION = 43,
-    NVIC_USB,
-    NVIC_PWM_GEN_3,
-    NVIC_UDMA_SOFTWARE,
-    NVIC_UDMA_ERROR,
-    NVIC_ADC_1_SEQ_0,
-    NVIC_ADC_1_SEQ_1,
-    NVIC_ADC_1_SEQ_2,
-    NVIC_ADC_1_SEQ_3,
-    NVIC_SSI_2 = 57,
-    NVIC_SSI_3,
-    NVIC_UART_3,
-    NVIC_UART_4,
-    NVIC_UART_5,
-    NVIC_UART_6,
-    NVIC_UART_7,
-    NVIC_I2C_2 = 68,
-    NVIC_I2C_3,
-    NVIC_16_32_TIMER_4_A,
-    NVIC_16_32_TIMER_4_B,
-    NVIC_16_32_TIMER_5_A = 92,
-    NVIC_16_32_TIMER_5_B,
-    NVIC_32_64_TIMER_0_A,
-    NVIC_32_64_TIMER_0_B,
-    NVIC_32_64_TIMER_1_A,
-    NVIC_32_64_TIMER_1_B,
-    NVIC_32_64_TIMER_2_A,
-    NVIC_32_64_TIMER_2_B,
-    NVIC_32_64_TIMER_3_A,
-    NVIC_32_64_TIMER_3_B,
-    NVIC_32_64_TIMER_4_A,
-    NVIC_32_64_TIMER_4_B,
-    NVIC_32_64_TIMER_5_A,
-    NVIC_32_64_TIMER_5_B,
-    NVIC_SYSTEM_EXCEPTION,
-    NVIC_PWM_1_GEN_0 = 134,
-    NVIC_PWM_1_GEN_1,
-    NVIC_PWM_1_GEN_2,
-    NVIC_PWM_1_GEN_3,
-    NVIC_PWM_1_FAULT
-} Nvic_InterruptsType;
+
+#define    NVIC_GPIO_PORT_A     0
+#define    NVIC_GPIO_PORT_B     1
+#define    NVIC_GPIO_PORT_C     2
+#define    NVIC_GPIO_PORT_D     3
+#define    NVIC_GPIO_PORT_E     4
+#define    NVIC_UART_0          5
+#define    NVIC_UART_1          6
+#define    NVIC_SSI_0           7
+#define    NVIC_I2C_0           8
+#define    NVIC_PWM_0_FAULT     9
+#define    NVIC_PWM_0_GEN_0     10
+#define    NVIC_PWM_0_GEN_1     11
+#define    NVIC_PWM_0_GEN_2     12
+#define    NVIC_QEI_0           13
+#define    NVIC_ADC_0_SEQ_0     14
+#define    NVIC_ADC_0_SEQ_1     15
+#define    NVIC_ADC_0_SEQ_2     16
+#define    NVIC_ADC_0_SEQ_3     17
+#define    NVIC_TIMER_WATCHDOG_0_1  18
+#define    NVIC_16_32_TIMER_0_A 19
+#define    NVIC_16_32_TIMER_0_B 20
+#define    NVIC_16_32_TIMER_1_A 21
+#define    NVIC_16_32_TIMER_1_B 22
+#define    NVIC_16_32_TIMER_2_A 23
+#define    NVIC_16_32_TIMER_2_B 24
+#define    NVIC_ANALOG_COMPARATOR_0 25
+#define    NVIC_ANALOG_COMPARATOR_1 26
+#define    NVIC_SYSTEM_CONTROL  28
+#define    NVIC_FLASH_EEPROM_MEMORY_CONTROL 27
+#define   NVIC_GPIO_PORT_F      28
+#define    NVIC_UART_2          33
+#define    NVIC_SSI_1           34
+#define    NVIC_16_32_TIMER_3_A 35
+#define    NVIC_16_32_TIMER_3_B 36
+#define    NVIC_I2C_1           37
+#define    NVIC_QEI_1           38
+#define    NVIC_CAN_0           39
+#define    NVIC_CAN_1           40
+#define    NVIC_HIBERNATION     43
+#define    NVIC_USB             44
+#define    NVIC_PWM_GEN_3       45
+#define    NVIC_UDMA_SOFTWARE   46
+#define    NVIC_UDMA_ERROR      47
+#define    NVIC_ADC_1_SEQ_0     48
+#define    NVIC_ADC_1_SEQ_1     49
+#define    NVIC_ADC_1_SEQ_2     50
+#define    NVIC_ADC_1_SEQ_3     51
+#define    NVIC_SSI_2           57
+#define    NVIC_SSI_3           58
+#define    NVIC_UART_3          59
+#define    NVIC_UART_4          60
+#define    NVIC_UART_5          61
+#define    NVIC_UART_6          62
+#define    NVIC_UART_7          63
+#define    NVIC_I2C_2           68
+#define    NVIC_I2C_3           69
+#define    NVIC_16_32_TIMER_4_A 70
+#define    NVIC_16_32_TIMER_4_B 71
+#define    NVIC_16_32_TIMER_5_A 92
+#define    NVIC_16_32_TIMER_5_B 93
+#define    NVIC_32_64_TIMER_0_A 93
+#define    NVIC_32_64_TIMER_0_B 94
+#define    NVIC_32_64_TIMER_1_A 95
+#define    NVIC_32_64_TIMER_1_B 96
+#define    NVIC_32_64_TIMER_2_A 97
+#define    NVIC_32_64_TIMER_2_B 98
+#define    NVIC_32_64_TIMER_3_A 99
+#define    NVIC_32_64_TIMER_3_B 100
+#define    NVIC_32_64_TIMER_4_A 101
+#define    NVIC_32_64_TIMER_4_B 102
+#define    NVIC_32_64_TIMER_5_A 103
+#define    NVIC_32_64_TIMER_5_B 104
+#define    NVIC_SYSTEM_EXCEPTION 105
+#define    NVIC_PWM_1_GEN_0     134
+#define    NVIC_PWM_1_GEN_1     135
+#define    NVIC_PWM_1_GEN_2     136
+#define    NVIC_PWM_1_GEN_3     137
+#define    NVIC_PWM_1_FAULT     138
+
 
 typedef struct
 {
-    Nvic_InterruptsType vector;     // name from Nvic_InterruptsType in IntCtlc.h
-    uint32 groupPriority;           // Group priorities set according to PRI_Group
-    uint32 subGroupPriority;        // Subgroup priorities set according to PRI_Group
+    uint32 vector;                   // interrupt name
+    uint32 groupPriority;           // Group priorities 
+    uint32 subGroupPriority;        // Subgroup priorities
 } NVIC_ConfigType;
 
 #endif /* NVIC_TYPES_H */
